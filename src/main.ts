0b8938d1193f53d0f73397c1f0738126bf1bcdb5
bekzod <bekzodbaratov454@gmail.com>
2026-06-16 22:30:18 +0500

    Loyihani yangilash

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dns from 'dns';
 
-
-import dns from "node:dns/promises";
-dns.setServers(["8.8.8.8" , "1.1.1.1"]);
-
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 
 async function bootstrap() {
